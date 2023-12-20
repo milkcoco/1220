cf935ff6e939db39dcbcf38107daf54c8186a4e9
milkcoco <lin0968329658@gmail.com>
2023-12-20 13:32:43 +0800

    練習2 - 使用互動物件

---
+++ exercise/server.R
@@ -0,0 +1,33 @@
+library(shiny)
+
+shinyServer(function(input, output) {
+  
+  DATA = reactive({
+    if (is.null(input$files)) {return()} else {
+      if(input$action1 == 0) {return()} else {
+        dat = read.table(input$files$datapath,header=T)
+        return(dat) 
+      }
+    }
+  })
+  
+  output$scatterPlot = renderPlot({
+    dat = DATA()
+    if (is.null(dat)) {return()} else {
+      plot(dat,col=input$Color)
+    }
+  })
+  
+  output$download = downloadHandler(
+    filename = function() {'plot.pdf'},
+    content = function(con) {
+      dat = DATA()
+      if (is.null(dat)) {return()} else {
+        pdf(con)
+        plot(dat,col=input$Color)
+        dev.off()
+      }
+    }
+  )
+  
+})
---
+++ exercise/ui.R
@@ -0,0 +1,19 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  
+  headerPanel("Ploting your data"), 
+  
+  sidebarPanel(
+    fileInput(inputId="files", label=h4("Upload your data file:"), multiple=FALSE, accept="text/plain"),
+    helpText("Note: you only can upload the .txt file."),
+    actionButton("action1", label = "畫圖"),
+    radioButtons("Color", "Select the color of histogram:", choices = c("Red" = "red", "Blue" = "blue", "Green" = "green"))
+  ),
+  
+  mainPanel(
+    plotOutput("scatterPlot"),
+    downloadButton("download", label = "Download plot", class = NULL)
+  )  
+  
+))
