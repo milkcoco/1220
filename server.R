994c7e844695a66a2f92d9f8661ef6b5854abc5a
milkcoco <lin0968329658@gmail.com>
2023-12-27 13:40:57 +0800

    使控制區的參數隨著Server端的狀況而改變

--- server.R
+++ server.R
@@ -2,32 +2,47 @@ library(shiny)
 
 shinyServer(function(input, output) {
   
-  DATA = reactive({
+  DATA <- reactive({
     if (is.null(input$files)) {return()} else {
-      dat = read.table(input$files$datapath,header=T)
+      dat <- read.table(input$files$datapath,header=T)
       return(dat) 
     }
   })
   
-  output$summary = renderPrint({
+  output$choose_columns1 <- renderUI({  #這裡是關鍵
     dat = DATA()
-    if (is.null(dat)) {return("You have to up load your data!!!")} else {
-      summary(dat)
+    if (is.null(dat)) {return()} else {
+      colnames <- colnames(dat)
+      selectInput("Y", h4("Choose a dependence variable:"), choices = colnames)
     }
   })
   
-  output$scatterPlot = renderPlot({
+  output$choose_columns2 <- renderUI({  #這裡是關鍵
     dat = DATA()
-    if (is.null(dat)) {return()} else {
-      plot(dat,col=input$Color)
+    if (is.null(dat)|is.null(input$Y)) {return()} else {
+      colnames <- colnames(dat)
+      selectInput("X", h4("Choose a independence variable:"), choices = colnames[which(colnames!=input$Y)])
     }
   })
   
-  output$view = renderTable({
+  output$summary <- renderPrint({
     dat = DATA()
-    if (is.null(dat)) {return()} else {
-      head(dat,input$n) 
-    }
+    if (is.null(dat)|is.null(input$Y)|is.null(input$X)) {return()} else {
+      X <- dat[,input$X]  #這裡是關鍵
+      Y <- dat[,input$Y]  #這裡是關鍵
+      Result=lm(Y~X)
+      return(summary(Result))
+    }  
+  })
+  
+  output$plot <- renderPlot({
+    dat = DATA()
+    if (is.null(dat)|is.null(input$Y)|is.null(input$X)) {return()} else {
+      X <- dat[,input$X]  #這裡是關鍵
+      Y <- dat[,input$Y]  #這裡是關鍵
+      plot(X,Y,pch=19)
+      abline(lm(Y~X),col="black")   
+    }  
   })
   
 })
--- ui.R
+++ ui.R
@@ -2,19 +2,18 @@ library(shiny)
 
 shinyUI(pageWithSidebar(
   
-  headerPanel("Summarize your data"), 
+  headerPanel("Linear regression for two continuous variables."), 
   
   sidebarPanel(
     fileInput(inputId="files", label=h4("Upload your data file:"), multiple=FALSE, accept="text/plain"),
     helpText("Note: you only can upload the .txt file."),
-    sliderInput("n", h4("Select the observed rows:"), min=5, max=20, value=10),
-    radioButtons("Color", "Select the color of histogram:", choices = c("Red" = "red", "Blue" = "blue", "Green" = "green"))
+    uiOutput("choose_columns1"),   #這裡是關鍵
+    uiOutput("choose_columns2")    #這裡是關鍵
   ),
   
   mainPanel(
     verbatimTextOutput("summary"),
-    plotOutput("scatterPlot"),
-    tableOutput("view")  
+    plotOutput("plot",width = "500px", height = "500px")
   )  
   
 ))
